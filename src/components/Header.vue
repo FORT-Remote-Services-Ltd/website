fe1efc687db2117c733bb0d336eec3c1ffc98d30
Moses Kamau <moseskamau338@gmail.com>
2023-10-22 11:17:11 +0300

    fix hero image issue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
     <div class="relative z-20 bg-[#007675]">
       <div class="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
         <div>
-          <a href="#" class="flex">
+          <a href="/" class="flex">
             <span class="sr-only">FORT REMOTE</span>
             <img class="h-8 w-auto sm:h-10" src="/images/logo/logo-dark.svg" alt="FORT REMOTE" />
           </a>
